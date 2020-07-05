52411f024f88f7885fbbbeed84c1339514f02ea8
tenpadrummer <4460drums@gmail.com>
2020-07-05 20:12:41 +0900

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 255}
   # メールアドレスの大文字小文字を無視した一意性の検証
   validates :email, format: {with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
