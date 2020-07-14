4fb49e31669f1df1e7e101b3dad075dd47d4aca1
tenpadrummer <4460drums@gmail.com>
2020-07-14 13:20:57 +0900

    include session helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # Railsのセッション用ヘルパーを全コントローラで使用できるようにする
+  include SessionsHelper
 
   def hello
     render html: "hello, world!"
