8a1924864e27394c3a287795c14b1743e540fdca
tenpadrummer <4460drums@gmail.com>
2020-07-16 15:11:44 +0900

    set friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,9 +44,10 @@ class UsersController < ApplicationController
   # ログイン済みユーザーかどうか確認
   def logged_in_user
     unless logged_in?
-       flash[:danger] = "Please log in."
-       redirect_to login_url
-     end
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
   end
 
   # 正しいユーザーかどうか確認
