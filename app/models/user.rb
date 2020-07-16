cb60d55e07a2485380dc9b6051e692c33cc744dd
tenpadrummer <4460drums@gmail.com>
2020-07-16 16:01:08 +0900

    generate migaration for activate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # attr_accessorを使って「仮想の」属性を作成
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   # before_saveメソッドで、ユーザーをデータベースに保存する前にemail属性を強制的に小文字に変換
-  before_save {self.email = email.downcase}
+  # before_createコールバックは、ユーザーが作成される前に呼び出される
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -47,5 +49,18 @@ class User < ApplicationRecord
   # ユーザーのログイン情報を破棄する
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  # メールアドレスをすべて小文字にする
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # 有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
