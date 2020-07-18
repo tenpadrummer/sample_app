bbac7216a20d76f6303439e48033d679722394ed
tenpadrummer <4460drums@gmail.com>
2020-07-19 00:02:42 +0900

    add dependent: :destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   before_save   :downcase_email
   before_create :create_activation_digest
 
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
