0a465fa970e475825ff90f38c277cf1a649a5eb2
tenpadrummer <4460drums@gmail.com>
2020-07-18 23:57:08 +0900

    set association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   before_save   :downcase_email
   before_create :create_activation_digest
 
+  has_many :microposts
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, presence: true, length: {maximum: 50}
